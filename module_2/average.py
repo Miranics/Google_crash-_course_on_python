7790ffc9fbfc4c4e17b4421cb9df0a41382665c5
Miracle <002nasya@gmail.com>
2024-10-08 12:46:44 +0200

    proper annotation

--- module_2/average.py
+++ module_2/average.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # Practice writing some expressions and conversions yourself.
-# In this scenario, we have a directory with 5 files in it.  
+# In this scenario, we have a directory with 5 files in it.
 # Each file has a different size: 2048, 4357, 97658, 125, and 8.
-# Fill in the blanks to calculate the average file size by having Python add all the values for you, and then set the files variable to the number of files. 
+# Fill in the blanks to calculate the average file size by having Python add all the values for you, and then set the files variable to the number of files.
 # Finally, print the average file size.
-total = 2048 + 4357 + 97658  + 125 + 8
+total = 2048 + 4357 + 97658 + 125 + 8
 files = 5
 average = total / files
 print("The average size is: " + str(average))
