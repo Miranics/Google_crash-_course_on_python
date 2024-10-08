a6660ae65bb8442b963ae5e6a4e93f33e8bfb977
Miracle <002nasya@gmail.com>
2024-10-08 09:13:09 +0200

    expressions

--- module_2/Expressions_numbers_and_type_conversions.py
+++ module_2/Expressions_numbers_and_type_conversions.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+# this file prints the area of a triangle
 base = 6
 height = 3
-area = (base*height)/2
-print("The area of the triangle is: " + str(area)) 
+area = (base * height) / 2
+print("The area of the triangle is: " + str(area))
