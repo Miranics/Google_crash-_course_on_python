e749f40a483538388076315fd8c6be12441ac00a
Miracle <002nasya@gmail.com>
2024-10-10 15:21:54 +0200

    simp func. def

---
+++ module_2/defining_function.py/example_1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# this file defines a function 
+def greeting(name):
+    print("Hello, " + name)
+
+greeting("Nanen")
+greeting("Kayla")
---
+++ module_2/defining_function.py/example_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def greeting(name, department):
+    print("welcome, " + name)
+    print("you're part of the " + department)
+
+greeting("Nanen", "AI & ML")
+greeting("Joe", "Mechatronics")
