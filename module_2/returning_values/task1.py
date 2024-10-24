4137a9c7dad4a4ea99cd89295ece64db67771359
Miracle <002nasya@gmail.com>
2024-10-24 15:55:57 +0200

    file update

---
+++ module_2/returning_values/task1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def area_triangle(base, height):
+    return base*height/2
+area_a = area_triangle(5,4)
+area_b = area_triangle(7,3)
+sum = area_a + area_b
+print("The sum of the two triangles is", str(sum))
